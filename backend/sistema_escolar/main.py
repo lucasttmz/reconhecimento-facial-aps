dcb1ceeb79697bf1e3c42c005e5b0920f7e0d882
lucasttmz <lucasttmz@gmail.com>
2024-09-11 19:20:43 -0300

    Adicionado CORS

--- backend/sistema_escolar/main.py
+++ backend/sistema_escolar/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from sistema_escolar.controladores import boletim, materias, usuarios
 
 
 app = FastAPI(title="Sistema Escolar")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost",],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(usuarios.router)
 app.include_router(materias.router)
 app.include_router(boletim.router)
