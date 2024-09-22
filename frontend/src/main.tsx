35948fc4b870620c868ccdad86273558a1f2ca1a
Vinicius Libano <34941776+ViniciusLibano@users.noreply.github.com>
2024-09-21 23:37:41 -0300

    Revert "Merge branch 'main' of https://github.com/lucasttmz/reconhecimento-facial-aps"
    This reverts commit 2613cf9221f4e16eb50020c8d6fe026b50b5217b, reversing
    changes made to 7f93b5978bc822f6d894dce1a0d0c5f8553940c1.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
