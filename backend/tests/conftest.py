45e5bd76e906e8e1c9d71a65dcdb0ec066b79c0d
lucasttmz <lucasttmz@gmail.com>
2024-10-19 01:25:58 -0300

    Facilitado o teste de login/registro

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,6 +3,7 @@ from random import randint
 
 import cv2
 from fastapi.testclient import TestClient
+import numpy as np
 import pytest
 
 from sistema_escolar.main import app
@@ -19,15 +20,18 @@ def aluno():
     token = criar_jwt(1).get("token")
     return {"Authorization": f"Bearer {token}"}
 
+
 @pytest.fixture(scope="session")
 def professor():
     token = criar_jwt(2).get("token")
     return {"Authorization": f"Bearer {token}"}
 
+
 @pytest.fixture(scope="session")
 def diretor():
     token = criar_jwt(3).get("token")
     return {"Authorization": f"Bearer {token}"}
+
 
 @pytest.fixture
 def materia_valida():
@@ -39,6 +43,7 @@ def materia_valida():
         "codigo_alunos": []
     }
 
+
 @pytest.fixture
 def notas_e_faltas():
     return {"faltas": randint(0, 10), "nota": randint(0, 10)}
@@ -48,9 +53,11 @@ def notas_e_faltas():
 def alteracao_de_cargo():
     return {"tipo": randint(1, 3)}
 
+
 @pytest.fixture(scope="session")
 def imagem_base64():
-    imagem = cv2.imread("tests/img/b64.jpg")
+    # Imagem de um pixel só para ser válida
+    imagem = np.array([[[0, 0, 255]]], dtype=np.uint8)
     _, buffer = cv2.imencode('.jpg', imagem)
     imagem_base64 = base64.b64encode(buffer).decode('utf-8')
 
